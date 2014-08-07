5066e2a4aebcc89725a12af82767b9af7b443400
Juan Edi <jedi@manas.com.ar>
2014-08-07 16:50:14 -0300

    #7074: tidy up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -459,11 +459,10 @@ module.exports = function (grunt) {
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
-    } else {
-      grunt.task.run(['ngconstant'])
     }
-
+    
     grunt.task.run([
+      'ngconstant',
       'clean:server',
       'configureProxies',
       'wiredep',
